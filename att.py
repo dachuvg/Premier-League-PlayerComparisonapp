4d59288e0caf2fc5c4e51d93e84d340ca0aa7476
Darshan Venugopal <darshanvenu04@gmail.com>
2025-01-02 00:43:32 +0400

    Updated app.py

--- att.py
+++ att.py
@@ -8,7 +8,9 @@ from selenium.webdriver.chrome.options import Options
 from scipy import stats
 import math
 import streamlit as st
+import chromedriver_autoinstaller
 
+chromedriver_autoinstaller.install()
 @st.cache_data 
 def get_att_data():
     import streamlit as st
